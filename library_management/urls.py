4d0ac88b99001fbca8bfaf7d802d30fb06723636
toaha-unc <toahasiddique.ts@gmail.com>
2025-08-17 17:54:40 -0500

    add redeployment changes

--- library_management/urls.py
+++ library_management/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.shortcuts import redirect
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -34,6 +35,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', lambda request: redirect('/swagger/'), name='home'),
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
