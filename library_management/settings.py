b40deb465ac948f52617f91b3041f98ea702c5cd
toaha-unc <toahasiddique.ts@gmail.com>
2025-08-17 17:56:23 -0500

    modify deployment changes

--- library_management/settings.py
+++ library_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a$sl9s&jny(5*yvg&stjdj75a_4shbevzjcea$1a7^#xye)!co
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*.vercel.app", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
